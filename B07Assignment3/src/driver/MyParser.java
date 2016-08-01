1b9c28788efc74e31240fff3e469d8faa44dbc5a
sivara57@UTORONTO.CA <sivara57@UTORONTO.CA@78374c2b-1ea4-4353-b2ea-0876c5cf414d>
2016-08-01 23:43:27 +0000

    updated MyPraser to handle the options between outputting to a file and or printing on screen

--- B07Assignment3/src/driver/MyParser.java
+++ B07Assignment3/src/driver/MyParser.java
@@ -18,33 +18,14 @@ public class MyParser {
 * @param args
 */
 public static void main(String[] args) {
- DEBUGStarterCode(args);
-}
-
-
-/*
-* This is a debug/helper method to help you get started. Once you understand
-* how this method is being used, you are free to refactor it, modify it, or
-* change it, or remove it entirely in any way you like.
-*/
-private static void DEBUGStarterCode(String[] args) {
- try {
-   System.out.println("DEBUG: URLS are " + args[0]);
-   System.out.println("DEBUG: FileName is " + args[1]);
- } catch (Exception e) {
-   System.out.println("Did you change the run configuration in"
-       + "Eclipse for argument0 and argument 1?");
+  String outputString = "";
+  outputString = outputType.PrintOutput.outputString(args);
+  if (args.length >= 2){
+    outputType.FileOutput.outputToFile(outputString, args[1]);
  }
-
-
- // TODO Auto-generated method stub
- String inputFiles[] = args[0].split(",");
- for (String inputFile : inputFiles) {
-   Extractor.ExtractAuthorNames.extractAuthorsName(inputFile);
-   Extractor.ExtractCitations.extractCitationValue(inputFile);
-   Extractor.Extracti10Index.extractIndexValue(inputFile);
- }
+  else{
+    System.out.print(outputString);
+  }
 }
-
 
 }
