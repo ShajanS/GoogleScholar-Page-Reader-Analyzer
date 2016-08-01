7e9c4a8ce218c8c12f7dd773abaee47e715ea518
sivara57@UTORONTO.CA <sivara57@UTORONTO.CA@78374c2b-1ea4-4353-b2ea-0876c5cf414d>
2016-08-01 23:45:46 +0000

    created a method to extract the number of total coAuthors present

---
+++ B07Assignment3/src/Extractor/ExtractCoAuthors.java
@@ -0,0 +1,35 @@
+package Extractor;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ExtractCoAuthors {
+  
+  public static String extractCoAuthorNames(String googleScholarURL) {
+    try {
+      
+      int coAuthorCount = 0;
+      ArrayList<String> coAuthorList = new ArrayList<String>();
+      
+      String rawHTMLString = ExtractHtml.getHTML(googleScholarURL);
+      String stringRegex =  "<a class=\"cit-dark-link\" "
+          + "href=\".*?hl=en\" title=\".*?\">(.*?)</a>";
+      Pattern patternObject = Pattern.compile(stringRegex);
+      Matcher matcherObject = patternObject.matcher(rawHTMLString);
+      
+      while (matcherObject.find()) {
+        coAuthorList.add(matcherObject.group(1));
+        coAuthorCount += 1;
+        
+      }
+      
+      return Integer.toString(coAuthorCount);
+
+    } catch (Exception e) {
+      System.out.println("malformed URL or cannot open connection to "
+          + "given URL");
+    }
+    return null;
+   }
+  
+}
