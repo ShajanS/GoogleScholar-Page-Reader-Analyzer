9b2e61576ac3658a285ffed48ed627e368885205
sivara57@UTORONTO.CA <sivara57@UTORONTO.CA@78374c2b-1ea4-4353-b2ea-0876c5cf414d>
2016-08-01 23:57:32 +0000

    renamed main package

--- B07Assignment3/src/extractor/ExtractAuthorNames.java
+++ B07Assignment3/src/extractor/ExtractAuthorNames.java
@@ -1,11 +1,11 @@
-package Extractor;
+package extractor;
 
 public class ExtractAuthorNames {
   
   public static String extractAuthorsName(String googleScholarURL) {
       String regexHtml = "<span id=\"cit-name-display\" "
           + "class=\"cit-in-place-nohover\">(.*?)</span>";
-      return Extractor.RegexCompiler.extractCodeInfo(regexHtml, googleScholarURL);
+      return extractor.RegexCompiler.extractCodeInfo(regexHtml, googleScholarURL);
    }
   
 }
--- B07Assignment3/src/extractor/Extracti10Index.java
+++ B07Assignment3/src/extractor/Extracti10Index.java
@@ -1,11 +1,11 @@
-package Extractor;
+package extractor;
 
 public class Extracti10Index {
   
   public static String extractIndexValue(String googleScholarURL) {
     String regexHtml = ">i10-index</a>" + "</td>" + 
         "<td class=\"cit-borderleft cit-data\">(.*?)</td>";
-    return Extractor.RegexCompiler.extractCodeInfo(regexHtml, googleScholarURL);
+    return extractor.RegexCompiler.extractCodeInfo(regexHtml, googleScholarURL);
     
    }
   
