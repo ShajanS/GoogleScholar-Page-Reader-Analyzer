731bcad4bad42527a440c5291fb8d2328b7ce45a
sivara57 <sivara57@UTORONTO.CA>
2016-08-01 03:23:23 +0000

    fixed regex html code issue

--- src/Extractor/ExtractCitations.java
+++ src/Extractor/ExtractCitations.java
@@ -3,7 +3,8 @@ package Extractor;
 public class ExtractCitations {
   
   public static void extractCitationValue(String googleScholarURL) {
-    String regexHtml = "<td class=\"cit-borderleft cit-data\">(.*?)</td>";
+    String regexHtml = ">Citations</a>" + "</td>" + 
+  "<td class=\"cit-borderleft cit-data\">(.*?)</td>";
     Extractor.RegexCompiler.extractCodeInfo(regexHtml, googleScholarURL);
     
    }
