656ac1d8bcc8dbb45e04c4d5cb44d23b48ef9410
sivara57@UTORONTO.CA <sivara57@UTORONTO.CA@78374c2b-1ea4-4353-b2ea-0876c5cf414d>
2016-08-01 23:47:06 +0000

    tweaked the extracting citation classes to meet req of assignment(total # of the first 5 citations)

--- B07Assignment3/src/Extractor/ExtractCitations.java
+++ B07Assignment3/src/Extractor/ExtractCitations.java
@@ -2,7 +2,7 @@ package Extractor;
 
 public class ExtractCitations {
   
-  public static String extractCitationValue(String googleScholarURL) {
+  public static String extractAllCitationValue(String googleScholarURL) {
     String regexHtml = ">Citations</a>" + "</td>" + 
   "<td class=\"cit-borderleft cit-data\">(.*?)</td>";
     return Extractor.RegexCompiler.extractCodeInfo(regexHtml, googleScholarURL);
