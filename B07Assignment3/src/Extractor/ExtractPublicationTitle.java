80222b3a24c7bf81e1c9785a9ea4a4ebc9c3c53b
sivara57@UTORONTO.CA <sivara57@UTORONTO.CA@78374c2b-1ea4-4353-b2ea-0876c5cf414d>
2016-08-01 23:44:20 +0000

    tweaked extractpublicationtitle to only return the title for the first 3

--- B07Assignment3/src/Extractor/ExtractPublicationTitle.java
+++ B07Assignment3/src/Extractor/ExtractPublicationTitle.java
@@ -1,12 +1,30 @@
 package Extractor;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ExtractPublicationTitle {
 
-  public static String extractTitle(String googleScholarURL) {
-    String regexHtml =
-        "<td id=\"col-title\"><a href=\".*?\" "
-        + "class=\"cit-dark-large-link\">(.*?)</a>";
-    return Extractor.RegexCompiler.extractCodeInfo(regexHtml, googleScholarURL);
+  public static String[] extractTitle(String googleScholarURL) {
+    int titleCounter = 0;
+    String[] firstThreeTitles = new String[3];
+    try {
+      String rawHTMLString = ExtractHtml.getHTML(googleScholarURL);
+      String stringRegex = "<td id=\"col-title\"><a href=\".*?\" "
+          + "class=\"cit-dark-large-link\">(.*?)</a>";
+      Pattern patternObject = Pattern.compile(stringRegex);
+      Matcher matcherObject = patternObject.matcher(rawHTMLString);
+      
+      while (matcherObject.find() && titleCounter < 3) {
+        firstThreeTitles[titleCounter] = matcherObject.group(1);
+        titleCounter ++;
+      }
+      return firstThreeTitles;
+
+    } catch (Exception e) {
+      System.out.print("malformed URL or cannot open connection to given URL");
+    }
+    return null;
 
   }
 
