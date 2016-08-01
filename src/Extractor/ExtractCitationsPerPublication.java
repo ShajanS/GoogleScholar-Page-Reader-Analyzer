46d6972aae8e5ab25bd96bead3ea6ddda216e044
sivara57 <sivara57@UTORONTO.CA>
2016-08-01 23:47:06 +0000

    tweaked the extracting citation classes to meet req of assignment(total # of the first 5 citations)

--- src/Extractor/ExtractCitationsPerPublication.java
+++ src/Extractor/ExtractCitationsPerPublication.java
@@ -1,11 +1,29 @@
 package Extractor;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ExtractCitationsPerPublication {
 
   public static String extractTotalCitations(String googleScholarURL) {
-    String regexHtml = "<td id=\"col-citedby\">"
-        + "<a class=\"cit-dark-link\" href=\".*?\">(.*?)</a></td>";
-    return Extractor.RegexCompiler.extractCodeInfo(regexHtml, googleScholarURL);
+    int totalCitations = 0;
+    int publicationCounter = 0;
+    try {
+      String rawHTMLString = ExtractHtml.getHTML(googleScholarURL);
+      String stringRegex = "<td id=\"col-citedby\">"
+          + "<a class=\"cit-dark-link\" href=\".*?\">(.*?)</a></td>";
+      Pattern patternObject = Pattern.compile(stringRegex);
+      Matcher matcherObject = patternObject.matcher(rawHTMLString);
+      
+      while (matcherObject.find() && publicationCounter < 5) {
+        totalCitations += Integer.parseInt(matcherObject.group(1));
+        publicationCounter ++;
+      }
+      return Integer.toString(totalCitations);
+
+    } catch (Exception e) {
+      return "malformed URL or cannot open connection to given URL";
+    }
 
   }
 
