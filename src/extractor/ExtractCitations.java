a206a5c6676d135b3bc1d32a35c6850a677306b2
sivara57 <sivara57@UTORONTO.CA>
2016-08-01 23:57:32 +0000

    renamed main package

--- src/extractor/ExtractCitations.java
+++ src/extractor/ExtractCitations.java
@@ -1,11 +1,11 @@
-package Extractor;
+package extractor;
 
 public class ExtractCitations {
   
   public static String extractAllCitationValue(String googleScholarURL) {
     String regexHtml = ">Citations</a>" + "</td>" + 
   "<td class=\"cit-borderleft cit-data\">(.*?)</td>";
-    return Extractor.RegexCompiler.extractCodeInfo(regexHtml, googleScholarURL);
+    return extractor.RegexCompiler.extractCodeInfo(regexHtml, googleScholarURL);
     
    }
   
--- src/outputType/PrintOutput.java
+++ src/outputType/PrintOutput.java
@@ -9,17 +9,17 @@ public class PrintOutput{
     int totalCoAuthors = 0;
     String inputFiles[] = args[0].split(",");
     for (String inputFile : inputFiles){
-      String[] titles = Extractor.ExtractPublicationTitle.extractTitle(inputFile);
+      String[] titles = extractor.ExtractPublicationTitle.extractTitle(inputFile);
       formatedString += 
           "-----------------------------------------------------------------------\n"
-      + "1. Name of Author:\n\t" + Extractor.ExtractAuthorNames.extractAuthorsName(inputFile) + "\n"
-      + "2. Number of All Citations:\n\t" + Extractor.ExtractCitations.extractAllCitationValue(inputFile) + "\n"
-      + "3. Number of i10-index after 2009:\n\t" + Extractor.Extracti10Index.extractIndexValue(inputFile) +"\n"
+      + "1. Name of Author:\n\t" + extractor.ExtractAuthorNames.extractAuthorsName(inputFile) + "\n"
+      + "2. Number of All Citations:\n\t" + extractor.ExtractCitations.extractAllCitationValue(inputFile) + "\n"
+      + "3. Number of i10-index after 2009:\n\t" + extractor.Extracti10Index.extractIndexValue(inputFile) +"\n"
       + "4. Title of the first 3 publications:\n\t" + "1-   " + titles[0] +"\n"+ "\t2-   " + titles[1] +"\n"+"\t3-   " + titles[2] +"\n"
-      + "5. Total paper citation (first 5 papers):\n\t" + Extractor.ExtractCitationsPerPublication.extractTotalCitations(inputFile) +"\n"
-      + "6. Total Co-Authors:\n\t" + Extractor.ExtractCoAuthors.extractCoAuthorNames(inputFile) +"\n";
+      + "5. Total paper citation (first 5 papers):\n\t" + extractor.ExtractCitationsPerPublication.extractTotalCitations(inputFile) +"\n"
+      + "6. Total Co-Authors:\n\t" + extractor.ExtractCoAuthors.extractCoAuthorNames(inputFile) +"\n";
       
-      totalCoAuthors += Integer.parseInt(Extractor.ExtractCoAuthors.extractCoAuthorNames(inputFile));
+      totalCoAuthors += Integer.parseInt(extractor.ExtractCoAuthors.extractCoAuthorNames(inputFile));
 
       if (inputFile == inputFiles[inputFiles.length-1]){
         formatedString +=  "-----------------------------------------------------------------------\n"
