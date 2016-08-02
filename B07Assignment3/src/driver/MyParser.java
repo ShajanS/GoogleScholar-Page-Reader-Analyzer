1a08765b43446c3bfaa1c1d268b30504c4ac216a
sivara57@UTORONTO.CA <sivara57@UTORONTO.CA@78374c2b-1ea4-4353-b2ea-0876c5cf414d>
2016-08-02 02:48:10 +0000

    fixed if block condition for MyPraser

--- B07Assignment3/src/driver/MyParser.java
+++ B07Assignment3/src/driver/MyParser.java
@@ -19,15 +19,21 @@ package driver;
 public class MyParser {
 
   /**
-   * 
+   * Main function to excute the program
    * @param args
    */
   public static void main(String[] args) {
+    // create a variable to hold the output string
     String outputString = "";
+    // create the output string for the desired urls
     outputString = outputType.PrintOutput.outputString(args);
+    // if there are two arguments (One holding urls and the other holding
+    // a outputfile name) out the string to a file 
     if (args.length >= 2) {
+      // Call the method which outputs the stirng to a file
       outputType.FileOutput.outputToFile(outputString, args[1]);
     } else {
+      // output the string to the console
       System.out.print(outputString);
     }
   }
