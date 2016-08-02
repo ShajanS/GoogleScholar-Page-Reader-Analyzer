61d6e5f39f05c3f8448282752535a255ff567b12
sivara57 <sivara57@UTORONTO.CA>
2016-08-02 03:01:11 +0000

    more test cases added

--- src/test/ExtractCitationsPerPublicationTest.java
+++ src/test/ExtractCitationsPerPublicationTest.java
@@ -2,13 +2,37 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
+/**
+ * Test cases for ExtractCitationsPerPublication Class
+ * 
+ * @author Shajan Sivarajah
+ *
+ */
 public class ExtractCitationsPerPublicationTest {
+  String inputUrl;
+  int sizePublications;
+  String publicationCitation;
 
-  @Test
-  public void test() {
-    fail("Not yet implemented");
+  @Before
+  public void setUp() throws Exception {
+    inputUrl = "sample2.html";
+    publicationCitation = extractor.ExtractCitationsPerPublication
+        .extractTotalCitations(inputUrl);
+
   }
 
+  @Test
+  public void testTotalCitations() {
+    assertTrue(publicationCitation.equals("158"));
+  }
+
+
+  @Test
+  public void testZeroCitations() {
+    assertFalse(publicationCitation.equals("0"));
+
+  }
 }
