3ffdf04fae337c823bf89a5bec7062bf6810d7fe
sivara57@UTORONTO.CA <sivara57@UTORONTO.CA@78374c2b-1ea4-4353-b2ea-0876c5cf414d>
2016-08-02 02:47:41 +0000

    fixed error message return type

--- B07Assignment3/src/extractor/ExtractHtml.java
+++ B07Assignment3/src/extractor/ExtractHtml.java
@@ -19,7 +19,7 @@ public class ExtractHtml {
    * @return htlm source text
    * @throws Exception
    */
-  public static String getHTML(String urlString) throws Exception {
+  public static String extractHTML(String urlString) throws Exception {
     // create object to store html source text as it is being collected
     StringBuilder html = new StringBuilder();
     // open connection to given url
