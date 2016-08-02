c9dcfdf47bb74e8830ad10952241af44af566eb5
sivara57@UTORONTO.CA <sivara57@UTORONTO.CA@78374c2b-1ea4-4353-b2ea-0876c5cf414d>
2016-08-02 02:54:58 +0000

    formated to google req

--- B07Assignment3/src/test/ExtractCoAuthorNamesTest.java
+++ B07Assignment3/src/test/ExtractCoAuthorNamesTest.java
@@ -6,8 +6,10 @@ import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
+
 /**
  * Test cases for ExtractCoAuthorNames Class
+ * 
  * @author Shajan Sivarajah
  *
  */
@@ -15,24 +17,26 @@ public class ExtractCoAuthorNamesTest {
   String inputUrl;
   int sizeAuthors;
   ArrayList<String> coAuthor;
+
   @Before
-  public void setUp() throws Exception{
+  public void setUp() throws Exception {
     inputUrl = "sample2.html";
     coAuthor = extractor.ExtractCoAuthorNames.extractCoAuthorNames(inputUrl);
-    
+
   }
+
   @Test
   public void testCoAuthorListSize() {
     sizeAuthors = coAuthor.size();
     assertTrue(sizeAuthors == 14);
   }
-  
+
   @Test
   public void testCoAuthorName() {
     String firstIndex = coAuthor.get(0);
     assertTrue(firstIndex.equals("Blair MacIntyre"));
   }
-  
+
   @Test
   public void testEmptyNames() {
     assertFalse(coAuthor.equals(""));
